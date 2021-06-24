1c7d74dcfe46d2d4e5ffae41eda8250c5a86b8ce
david daniels <ddaniels7009@gmail.com>
2021-06-23 22:40:05 -0600

    Added boxes to contain each book in preperation for the delete function to be implemented

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 
-const viewButton = document.getElementById("viewBooks");
+
 const addButton = document.getElementById("addButton")
+
 const displayText = document.getElementById("display");
 const editSection = document.getElementById("editSection");
+const bookSection = document.getElementById("books");
 
 let unreadInput = document.createElement("input");
 let readInput = document.createElement("input");
 let pageInput = document.createElement("input");
 let submitButton = document.createElement("button");
 
+let form = document.createElement("form");////
+
 editSection.appendChild(submitButton);
-submitButton.classList.add("invisible");
+submitButton.classList.toggle("invisible");
 submitButton.innerHTML = "Add Book";
 submitButton.style.order = 3;
 
-
-
 let myLibrary = [];
 
+let count = 0;
+
+readInput.classList.toggle("input,input[type='radio'][value='Unread']");
 
 // Constructor for Book Objects
 function Book(title, author, numPages, read) {
@@ -28,10 +33,10 @@ function Book(title, author, numPages, read) {
 
     this.info = function () {
 
-        let output = "Title: "+ this.title + "\n" + 
-        "Author: " + this.author + "\n" +
-        "Pages: " + this.numPages + "\n" +
-        "Status: " + this.read;
+        let output = "Title: " + this.title + "\n" +
+            "Author: " + this.author + "\n" +
+            "Pages: " + this.numPages + "\n" +
+            "Status: " + this.read;
         return output;
 
     }
@@ -45,20 +50,38 @@ function addBookToLibrary(title, author, pages, read) {
     // Add book object to the end of the array
     myLibrary.push(new Book(title, author, pages, read));
 
+    // Create boxes to hold books
+    createBookBox();
+    
+
+}
+
+// Create book boxes and append to books div
+function createBookBox(){
+
+    let box = document.createElement("div");
+    bookSection.appendChild(box);
+    box.classList.add("boxes")
+    box.innerText = (myLibrary[count].info())
+    count++;
+// Just need to add 
 }
 
 function display(item) {
 
-    displayText.innerText += item.info() + "\n";
-    console.log(item.title);
-
-}
-
-function removeBook(){
-
+    displayText.innerText += item.info() + "\n" + "\n";
 
 
 }
+
+function removeBook(array) {
+
+    array.splice(0, 1);
+    
+    }
+    
+
+
 
 // Clear display each time user presses view collection button.
 function clearDisplay() {
@@ -68,9 +91,9 @@ function clearDisplay() {
 // Bring up a form when the add book button is pressed
 function createForm() {
 
-    let form = document.createElement("form");////
+
+
     editSection.appendChild(form);///////
-    
     form.classList.add("form");
 
     //Begin
@@ -127,58 +150,50 @@ function createForm() {
 
     readOption.setAttribute("for", "read");
     readOption.innerHTML = "Read";
-    
+
     form.appendChild(readInput);
     readInput.setAttribute("type", "radio");
     readInput.setAttribute("id", "read");
     readInput.setAttribute("name", "read");
-    readInput.setAttribute("value", ""); 
+    readInput.setAttribute("value", "Read")
     //End
+
 
 
     //Begin
     submitButton.addEventListener('click', function () { // Submit the book to be added
         addBookToLibrary((document.getElementById("title").value),
-        document.getElementById("author").value,
-        document.getElementById("pages").value,
-        document.getElementById("read").value);
+            document.getElementById("author").value,
+            document.getElementById("pages").value,
+            document.getElementById("read").value);
         submitButton.classList.toggle("invisible");
+        readInput.classList.toggle("input,input[type='radio'][value='Unread']");
         clearDisplay();
         myLibrary.forEach(display);
-        editSection.removeChild(form);
-        
+        //uncheck();
+        form.classList.toggle("invisible"); // keep
+
     })
     //End
 
-    
 
 }
 
+
+
 // Create a new book entry
-addButton.addEventListener('click', function () { createForm(); submitButton.classList.toggle("invisible");});
+addButton.addEventListener('click', function () { form.classList.toggle("invisible"); submitButton.classList.toggle("invisible"); });
+
+form.classList.toggle("invisible")
+createForm();
 
 //addButton.addEventListener('click', function () { createForm(); editSection.removeChild(form)});
 
 //viewButton.addEventListener('click', function() { clearDisplay(); myLibrary.forEach(display); })
 
 // call the addtolibrary function to add a book to the library
-//addBookToLibrary("The Hail Mary", "Andy Weir", 497, "not read yet");
-//addBookToLibrary("The House in the Cerulean Sea", "TJ Klune", 393, "not read yet");
-//addBookToLibrary("A Storm of Swords", "George R.R. Martin", 1008, "not read yet");
+addBookToLibrary("The Hail Mary", "Andy Weir", 497, "not read yet");
+addBookToLibrary("The House in the Cerulean Sea", "TJ Klune", 393, "not read yet");
+addBookToLibrary("A Storm of Swords", "George R.R. Martin", 1008, "not read yet");
 
 myLibrary.forEach(display);
-
-
-{/* 
-<form action="/action_page.php">
-    <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname" value="John"><br>
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname" value="Doe"><br><br>
-    <input type="submit" value="Submit">
-</form> 
-*/}
-
-
-// Use this to get the value contained in the input field when the sumbit button is pressed
-// document.getElementById('textbox_id').value
